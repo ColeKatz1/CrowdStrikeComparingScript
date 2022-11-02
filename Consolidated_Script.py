fc085d0a50eea68363cb108a8e09708277c2570b
ColeKatz1 <84477747+ColeKatz1@users.noreply.github.com>
2022-11-02 19:12:23 -0400

    Update Consolidated_Script.py

--- Consolidated_Script.py
+++ Consolidated_Script.py
@@ -3,12 +3,10 @@ from Mac_Lists import createMacCSVFiles
 from List_Filters import windowsFilterLastLoginDate
 from List_Filters_Mac import macsFilterLastLoginDate
 
-#createWindowsCSVFiles creates two lists. One is a list of devices that should have CS installed but don't show up in CS host management
-#The other is a list of devices that we have in our own database as not having CS installed, but they do show up in CS host management
-createWindowsCSVFiles("C:/Users/colek/OneDrive/Desktop/New folder/CS_Host_Data_8_11_22.csv","C:/Users/colek/OneDrive/Desktop/New folder/Compliance 2022-06_CrowdStrike.xlsx","8_12_22")
-#this creates the same lists as above but for macs instead of windows
-createMacCSVFiles("C:/Users/colek/OneDrive/Desktop/New folder/CS_Host_Data_8_11_22.csv","C:/Users/colek/OneDrive/Desktop/New folder/Compliance 2022-06_CrowdStrike.xlsx","8_11_22")
-#this filters for windows last active in a specified month
-windowsFilterLastLoginDate("C:/Users/colek/OneDrive/Desktop/New folder/Windows_Missing_From_CS_Database_8_11_22.csv", "C:/Users/colek/OneDrive/Desktop/New folder/Compliance 2022-06_CrowdStrike.xlsx", "Jul","2022","8_11_22")
-#this filters for macs last active in a specified month - the syntax of the month is slightly different (you put the month in number form like 07 for July)
-macsFilterLastLoginDate("C:/Users/colek/OneDrive/Desktop/New folder/Macs_Missing_From_CS_Database_8_11_22.csv", "C:/Users/colek/OneDrive/Desktop/New folder/Compliance 2022-06_CrowdStrike.xlsx", "07","2022","8_11_22")
+
+def consolidatedScript(CSHostFilePath, CornellDatabaseFilePath, date, filterMonth, filterMonthAsNumber, filterYear): #filterMonth would be "Jul" for example. filterMonthAsNumber would be "07" for July
+    createWindowsCSVFiles(CSHostFilePath, CornellDatabaseFilePath, date)
+    createMacCSVFiles(CSHostFilePath, CornellDatabaseFilePath, date)
+    windowsFilterLastLoginDate("Windows_Missing_From_CS_Database_" + date +".csv", CornellDatabaseFilePath, filterMonth, filterYear, date)
+    macsFilterLastLoginDate("Macs_Missing_From_CS_Database_" + date + ".csv", CornellDatabaseFilePath, filterMonthAsNumber,filterYear,date)
+
